80a0e81b4a1315728438ac9ed3ab8ec071a464c6
Lakshmi096 <lakshmirk.096@gmail.com>
2020-07-17 18:02:43 +0530

    Updates

---
+++ precache-manifest.a78ad1c33c87800f6015bcd192607ca3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ab93dbbdd12c7da715b4adfc24ab7a",
+    "url": "/react-tictactioe/index.html"
+  },
+  {
+    "revision": "691c07f37839e79ebbd9",
+    "url": "/react-tictactioe/static/css/2.5ccc65cc.chunk.css"
+  },
+  {
+    "revision": "5252c4e7fa17e63495d7",
+    "url": "/react-tictactioe/static/css/main.ab2dd69c.chunk.css"
+  },
+  {
+    "revision": "691c07f37839e79ebbd9",
+    "url": "/react-tictactioe/static/js/2.38482253.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-tictactioe/static/js/2.38482253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5252c4e7fa17e63495d7",
+    "url": "/react-tictactioe/static/js/main.e349cb08.chunk.js"
+  },
+  {
+    "revision": "360bb0fcda4a39b734bf",
+    "url": "/react-tictactioe/static/js/runtime-main.7935c10b.js"
+  }
+]);
